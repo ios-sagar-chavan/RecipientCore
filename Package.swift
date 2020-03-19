bcc2e55eae506adb6036e78f0feb6c6eeca70b3c
ios-sagar-chavan <61308390+ios-sagar-chavan@users.noreply.github.com>
2020-03-19 15:09:36 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
 
-        .package(url: "https://github.com/ios-sagar-chavan/APICore.git", .upToNextMajor(from: "1.0.4"))
+        .package(url: "https://github.com/ios-sagar-chavan/APICore.git", .upToNextMajor(from: "1.0.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
