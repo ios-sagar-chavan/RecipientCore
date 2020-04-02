3eb4ea4ab9f30d4ed327a6c267d2ea0f209aa82a
ios-sagar-chavan <61308390+ios-sagar-chavan@users.noreply.github.com>
2020-04-02 12:08:27 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RecipientCore",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,17 +13,17 @@ let package = Package(
             targets: ["RecipientCore"]),
     ],
     dependencies: [
-
-        .package(url: "https://github.com/ios-sagar-chavan/APICore.git", .upToNextMajor(from: "1.0.5"))
+        //.package(url: "https://github.com/ios-sagar-chavan/APICore.git", .upToNextMajor(from: "1.0.4"))
+        .package(url: "https://github.com/ios-sagar-chavan/Framework.git", .upToNextMajor(from: "1.0.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RecipientCore",
-            dependencies: ["APICore"]),
+            dependencies: ["Framework"]),
         .testTarget(
             name: "RecipientCoreTests",
-            dependencies: ["RecipientCore","APICore"]),
+            dependencies: ["RecipientCore","Framework"]),
     ]
 )
